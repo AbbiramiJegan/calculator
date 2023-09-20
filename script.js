7ecd791d1cdb36d0d61a7dcd75d1eff7bd2eff8d
AbbiramiJegan <108280861+AbbiramiJegan@users.noreply.github.com>
2023-09-20 21:44:13 +0800

    Created calculator class and functions

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+/* To store all the information of my calculator I create a class */
+class Calculator{
+    // Takes all the inputs and functions of my calculator
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear() // as soon as we create new calculator it gets set to its default values
+    }
+    
+    clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+
+    }
+}
+/* We have everything selected now */
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
